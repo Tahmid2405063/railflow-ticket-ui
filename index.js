14b30d5548422abf43839c16371a139d72d46ba8
arman <siratularman2@gmail.com>
2026-08-30 23:05:17 +0600

    ..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const expr = require("express");
 
-const userRouter = require("./routes/user");
+
 const trainRouter = require("./routes/train");
 const routesRouter = require("./routes/trainRoutes");
 
@@ -14,7 +14,6 @@ app.use(expr.urlencoded({ extended: false }));
 
 
 // Routes
-app.use("/api/user", userRouter);
 app.use("/api/train", trainRouter);
 app.use("/api/route", routesRouter);
 
